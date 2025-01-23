5c5a4f74c900478b56d6f613a76b3cd2936c3fd2
Іван <65630800+bahioiiia@users.noreply.github.com>
2025-01-24 01:05:36 +0200

    keys sizes

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './Key.module.css';
 
 function Key({ keyObj, isActive, isWorking, isPressed }) {
+  const keyClass = `${styles.key} ${isActive ? styles.active : ''} ${isWorking ? styles.working : ''} ${isPressed ? styles.pressed : ''} ${styles[keyObj.code.toLowerCase()] || ''}`;
+  
   return (
-    <div
-      className={`${styles.key} ${isActive ? styles.active : ''} ${isWorking ? styles.working : ''} ${isPressed ? styles.pressed : ''}`}
-    >
+    <div className={keyClass}>
       {keyObj.type === 'symbol' ? (
         <>
           <div className={styles.symbolBox}>{keyObj.labels.tl || ''}</div>
